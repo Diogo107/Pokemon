027135fbb1cf7749180339b86ae4f2baa02819d0
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-03-30 10:27:45 +0100

    Updates

---
+++ precache-manifest.5400b1b9e46417f7ccc91791c8189cae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36730101f8b99800e7efc28de7237de5",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "677ca365d3183883c4f8",
+    "url": "/Pokemon/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bfad6e066ea7694389ef",
+    "url": "/Pokemon/static/css/main.db65557d.chunk.css"
+  },
+  {
+    "revision": "677ca365d3183883c4f8",
+    "url": "/Pokemon/static/js/2.1a98bc0c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Pokemon/static/js/2.1a98bc0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfad6e066ea7694389ef",
+    "url": "/Pokemon/static/js/main.a3174891.chunk.js"
+  },
+  {
+    "revision": "fe097e39e1464985281a",
+    "url": "/Pokemon/static/js/runtime-main.c7103e98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pokemon/static/media/logo.5d5d9eef.svg"
+  }
+]);
