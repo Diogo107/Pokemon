a51c32fb844c79d3285e1a5c7f73feab51d3fb79
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-03-30 10:12:17 +0100

    build

---
+++ build/precache-manifest.8f5d14b952bfa5e8f355334b8c32b917.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c539d70a04f25abe0e141d83f1c3adf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "677ca365d3183883c4f8",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b9a00e1057f39c876a2a",
+    "url": "/static/css/main.db65557d.chunk.css"
+  },
+  {
+    "revision": "677ca365d3183883c4f8",
+    "url": "/static/js/2.1a98bc0c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1a98bc0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a00e1057f39c876a2a",
+    "url": "/static/js/main.61c8c2cc.chunk.js"
+  },
+  {
+    "revision": "213b024422a8e31df484",
+    "url": "/static/js/runtime-main.9afaafa4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
