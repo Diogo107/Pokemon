c2114a4af0a30879c24faf4bf02a3e0986ff8444
Diogo107 <55454913+Diogo107@users.noreply.github.com>
2020-03-30 10:18:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-
-const baseDir = `${__dirname}/build/`;
-
-app.use(express.static(`${baseDir}`));
-
-app.get('*', (req, res) => res.sendFile('index.html', { root: baseDir }));
-
-const port = 4000;
-
-app.listen(port, () => console.log(`Servidor subiu com sucesso em http://localhost:${port}`));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(9000);
